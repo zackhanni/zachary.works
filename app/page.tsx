5e3f4ae19406d83d42a82821419b6273125792c3
Zack Hanni <zack.hannI@gmail.com>
2023-10-02 16:31:52 -0400

    add hero to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Contact from "./components/Contact";
+import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
+      <Hero />
       <Projects />
       <Contact />
     </main>
