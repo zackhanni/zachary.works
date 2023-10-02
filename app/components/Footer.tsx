3d18e3623676e0c0be60463d9008f82d6e4aee2d
Zack Hanni <zack.hannI@gmail.com>
2023-10-02 16:32:20 -0400

    add basic footer content

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="flex items-center justify-center  bg-slate-600 text-white py-20 px-10">
+      <div className="flex">
+        <div className="left-side">
+          <div className="title font-bold text-2xl">Contact</div>
+          <div className="location">Philadelphia, PA</div>
+          <div className="phone-number">856-298-3095</div>
+          <div className="email">zack.hanni@gmail.com</div>
+          <div className="resume">Zack Resume</div>
+        </div>
+        <div className="right-side">
+          <div className="title font-bold text-2xl">About Me</div>
+          <div className="description max-w-xs">
+            I enjoy coding, learning, and audio engineering. I am open to
+            discussing new projects and design challenges.
+          </div>
+          <div className="social media"></div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
