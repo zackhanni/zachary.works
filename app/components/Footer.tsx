2fa16698de2e0cf6b056da1f8a8286d75ea2e5d7
Zack Hanni <zack.hannI@gmail.com>
2023-10-02 16:51:49 -0400

    update footer style, added icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,18 +1,18 @@
-
+import { FaRegEnvelope, FaHome,FaPhone, FaRegFileAlt } from 'react-icons/fa';
 
 function Footer() {
   return (
-    <footer className="flex items-center justify-center  bg-slate-600 text-white py-20 px-10">
-      <div className="flex">
-        <div className="left-side">
-          <div className="title font-bold text-2xl">Contact</div>
-          <div className="location">Philadelphia, PA</div>
-          <div className="phone-number">856-298-3095</div>
-          <div className="email">zack.hanni@gmail.com</div>
-          <div className="resume">Zack Resume</div>
+    <footer className="flex items-center justify-center  bg-slate-600 text-white py-20">
+      <div className="flex flex-col md:flex-row justify-between w-full max-w-[1200px] p-8 xl:p-0 ">
+        <div className="left-side pb-8 md:pb-0">
+          <div className="title font-bold text-2xl py-2">Contact</div>
+          <div className="location flex"><FaHome />Philadelphia, PA</div>
+          <div className="phone-number flex"><FaPhone />856-298-3095</div>
+          <div className="email flex"><FaRegEnvelope /> zack.hanni@gmail.com</div>
+          <div className="resume flex"><FaRegFileAlt />Zack Resume</div>
         </div>
         <div className="right-side">
-          <div className="title font-bold text-2xl">About Me</div>
+          <div className="title font-bold text-2xl py-2">About Me</div>
           <div className="description max-w-xs">
             I enjoy coding, learning, and audio engineering. I am open to
             discussing new projects and design challenges.
