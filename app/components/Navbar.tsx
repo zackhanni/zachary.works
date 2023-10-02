b2139ddc4d8b2558f022d0eac3e1138f92d043c5
Zack Hanni <zack.hannI@gmail.com>
2023-10-02 16:31:18 -0400

    begin simple navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="bg-black h-24 text-white grid place-content-center">
+      <Link href="/">
+        <h2 className=" text-5xl">Zacahry.works</h2>
+      </Link>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
