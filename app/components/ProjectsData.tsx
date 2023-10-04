76363fec2815fff17bf11937c1952c66fbc0eaea
Zack Hanni <zack.hannI@gmail.com>
2023-10-04 13:35:05 -0400

    add data file for projects

---
+++ app/components/ProjectsData.tsx
@@ -0,0 +1,60 @@
+const ProjectsData = [
+  {
+    src: "/soma-website.png",
+    title: "Recording Studio Website",
+    description: "I was hired by Soma to create a responsive service based website with SEO and load speed tests in mind. I deployed the page on netlify and handled DNS",
+    liveWebsite: "https://www.soundslikesoma.com/",
+    github: "https://github.com/zackhanni/soundslikesoma",
+  },
+  {
+    src: "/visual-impairment.png",
+    title: "Visual Impairment Simulator",
+    description: "A website designed to simulate various visual disabilities",
+    liveWebsite: "https://visual-impairment.netlify.app/",
+    github: "https://github.com/zackhanni/visual-impairments",
+  },
+  {
+    src: "/project3.png",
+    title: "Ecommerce Website Design",
+    description: "A product website for a clothing company selling a variety of clothing. Complete with item descriptions, size and color options, and a checkout page.",
+    liveWebsite: "https://zack-ecommerce.netlify.app/",
+    github: "https://github.com/zackhanni/ecommerce-website-demo",
+  },
+  {
+    src: "/UX-design-demo.png",
+    title: "UX and UI Design",
+    description: "I have experience designing products and apps with Figma. I have a seperate portfolio of designs and case studies featured on Dribble.com",
+    liveWebsite: "https://dribbble.com/zackhanni",
+    github: "https://dribbble.com/zackhanni",
+  },
+  {
+    src: "/emo-intel.png",
+    title: "Develop Emotional Intelligence",
+    description: "Empowering people with tools to describe their feelings in more detail",
+    liveWebsite: "https://how-are-you-feeling.vercel.app/",
+    github: "https://github.com/zackhanni/how-are-you-feeling",
+  },
+  {
+    src: "/dino.jpg",
+    title: "Dinosaur themed THE-saurus",
+    description: "I worked with Dictionary.com to access their thesaurus API. This app is deployed through Vercel",
+    liveWebsite: "https://the-saurus.vercel.app/",
+    github: "https://github.com/zackhanni/the-saurus",
+  },
+  {
+    src: "/weather.jpg",
+    title: "Weather search app",
+    description: "I accessed a weather API to find temperature highs, lows, humidity, etc in any city around the world. This app is deployed through Varcel",
+    liveWebsite: "https://next-weather-app-zackhanni.vercel.app/",
+    github: "https://github.com/zackhanni/weather-app-react",
+  },
+  {
+    src: "/calculator.png",
+    title: "Zack's Calculator",
+    description: "Are you one of the few who think using a built in capculator app is inconvenient? Look no further than my even more inconveniencing JS, HTML, and CSS based calculator app!",
+    liveWebsite: "https://zacks-calculator.netlify.app/",
+    github: "https://github.com/zackhanni/js-calculator-app",
+  },
+];
+
+export default ProjectsData;
