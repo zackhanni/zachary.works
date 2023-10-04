14fe0f55c840fc464575f1002baafe4f56505b45
Zack Hanni <zack.hannI@gmail.com>
2023-10-04 13:38:50 -0400

    remove home tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import Projects from "./components/Projects";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+    <main className="">
       <Hero />
       <Projects />
       <Contact />
