e58bf283d6d51e009cf464ac6b056aacb78f5af8
Zack Hanni <zack.hannI@gmail.com>
2023-10-04 13:37:14 -0400

    fix navbar to stay fixed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,10 +3,12 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className="bg-black h-24 text-white grid place-content-center">
-      <Link href="/">
-        <h2 className=" text-5xl">Zacahry.works</h2>
-      </Link>
+    <nav className="bg-black w-full h-24 text-white fixed top-0 z-50">
+      <div className="">
+        <Link href="/">
+          <h2 className="text-5xl text-center">Navbar</h2>
+        </Link>
+      </div>
     </nav>
   );
 }
