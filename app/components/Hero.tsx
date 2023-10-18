1869f70a36c999cdaa20eaff9834a4c92c74cacb
Zack Hanni <zack.hannI@gmail.com>
2023-10-18 11:50:54 -0400

    rainbow text

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,15 +2,8 @@ import React from "react";
 import Image from "next/image";
 
 function Hero() {
-  const HeroContent = [
-    {
-      largeText: "This is the large hero text",
-      smallText: "This is additional smaller text",
-    },
-  ];
-
   return (
-    <section className="h-screen w-full bg-black">
+    <section id="hero" className="h-screen w-full bg-black">
       <div className="">
         <Image
           src="/hero-image-table.jpg"
@@ -21,9 +14,8 @@ function Hero() {
           className=" opacity-20 absolute object-cover top-0 left-0"
         />
         <div className="content absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-          <p className="text-7xl text-white ">{HeroContent[0].largeText}</p>
-          <p className="text-xl text-white ">{HeroContent[0].smallText}</p>
-          <div>button link 1 and 2</div>
+          <p className="text-xl text-white uppercase">Hi, Im a <span id="rainbow" className="font-semibold" >software engineer</span>.</p>
+          <p className="text-6xl text-white font-bold">I built this site with Next.js</p>
         </div>
       </div>
     </section>
