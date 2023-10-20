6c149f0b2f5ba44ff4b30ea9a2ed5a0a2aa3f19a
Zack Hanni <zack.hannI@gmail.com>
2023-10-20 15:05:20 -0400

    prettier formatting

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -14,8 +14,16 @@ function Hero() {
           className=" opacity-20 absolute object-cover top-0 left-0"
         />
         <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-          <p className="text-2xl text-white uppercase">Hi, Im a <span id="rainbow" className="font-semibold" >software engineer</span>.</p>
-          <p className="text-6xl text-white font-bold">I built this site with Next.js</p>
+          <p className="text-2xl text-white uppercase">
+            Hi, Im a{" "}
+            <span id="rainbow" className="font-semibold">
+              software engineer
+            </span>
+            .
+          </p>
+          <p className="text-5xl md:text-7xl text-white font-bold">
+            I built this site with Next.js
+          </p>
         </div>
       </div>
     </section>
