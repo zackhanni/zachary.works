b8cfebd7b2a674437b0c609fbb80b0832c71ac18
Zack Hanni <zack.hannI@gmail.com>
2023-10-27 12:51:02 -0400

    remove navbar for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar />
+        {/* <Navbar /> */}
         {children}
         <Footer />
       </body>
