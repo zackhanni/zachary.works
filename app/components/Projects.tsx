d582f71927355c7f4940f257d96c68275f19ccf4
Zack Hanni <zack.hannI@gmail.com>
2023-10-30 12:01:56 -0400

    start setup for intersection observer

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,7 +1,25 @@
 import ProjectsData from "./ProjectsData";
 import Image from "next/image";
+import "../components/ProjectsStyles.css";
 
 function Projects() {
+  // const observer = new IntersectionObserver(
+  //   (entries) => {
+  //     entries.forEach((entry) => {
+  //       entry.target.classList.toggle("show", entry.isIntersecting);
+  //       if (entry.isIntersecting) observer.unobserve(entry.target);
+  //     });
+  //   },
+  //   {
+  //     threshold: 0.75,
+  //   }
+  // );
+
+  // const cards = document.querySelectorAll(".card")
+  // cards.forEach(card => {
+  //   observer.observe(card)
+  // })
+
   return (
     <section
       id="projects"
@@ -14,8 +32,7 @@ function Projects() {
         {ProjectsData.map((project) => {
           return (
             <li
-              id="project-card"
-              className="md:max-w-[350px] min-w-[300px] bg-slate-400/20 p-4 rounded-lg shadow-2xl"
+              className="card md:max-w-[350px] min-w-[300px] bg-slate-400/20 p-4 rounded-lg shadow-2xl"
               key={project.title}
             >
               <Image
@@ -25,19 +42,22 @@ function Projects() {
                 alt={project.title}
                 className="object-cover w-full h-[175px] rounded-sm"
               />
-              <div className="flex">
-                {project.tools?.map((tool) => {
-                  return (
-                    <p className="p-1 text-white" key={tool}>
-                      {tool}
-                    </p>
-                  );
-                })}
-              </div>
-              <div className="py-4 text-white">
+              <div className="py-3 text-white">
                 <div>
                   <p className="text-2xl font-bold">{project.title}</p>
-                  <p className="pb-4">{project.description}</p>
+                  <p className="py-6">{project.description}</p>
+                  <div className="flex flex-wrap pb-6">
+                    {project.tools?.map((tool) => {
+                      return (
+                        <p
+                          className="text-white bg-blue-800 rounded-full py-1 px-4 mx-1"
+                          key={tool}
+                        >
+                          {tool}
+                        </p>
+                      );
+                    })}
+                  </div>
                 </div>
                 <div id="project-buttons" className="flex justify-between">
                   <button className="px-6 py-3 bg-black hover:bg-zinc-800 rounded-sm font-semibold">
