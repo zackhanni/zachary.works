ae46eddcce14cc84dd0f1fca6905e2824011260c
Zack Hanni <zack.hannI@gmail.com>
2023-11-01 13:37:10 -0400

    remove unused contact component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 
@@ -7,7 +6,6 @@ export default function Home() {
     <main>
       <Hero />
       <Projects />
-      <Contact />
     </main>
   );
 }
