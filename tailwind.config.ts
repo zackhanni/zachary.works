b369cc0ae74ce77ee0e84e7563014e8780967bc9
Zack Hanni <zack.hannI@gmail.com>
2023-11-01 17:13:48 -0400

    create max width options

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,37 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+      'max-2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'max-xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'max-lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'max-md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'max-sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
   },
   plugins: [],
 }
