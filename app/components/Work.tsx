1fb786b1030dc4fa306a70466f1d3b1b12e065c8
Zack Hanni <zack.hannI@gmail.com>
2023-11-07 17:48:13 -0500

    show multiple paragraphs in description

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -12,23 +12,29 @@ export default function Work() {
           Work Experience
         </h2>
         <div className="min-w-[50%] max-w-[90%] lg:max-w-[50%]">
-        {WorkData.map((job) => {
-          return (
-            <div
-              key={job.company}
-              className="bg-slate-800/20 p-8 
+          {WorkData.map((job) => {
+            return (
+              <div
+                key={job.company}
+                className="bg-slate-800/20 p-8 
         mt-6 rounded-lg radial-gradient backdrop-blur"
-            >
-              <p className="text-2xl font-semibold">{job.title}</p>
-              <div className="flex text-white/80">
-                <p className="text-blue-600 mr-1">{job.company}</p>
-                <p>{job.dateRange}</p>
+              >
+                <p className="text-2xl font-semibold">{job.title}</p>
+                <div className="flex text-white/80">
+                  <p className="text-blue-600 mr-1">{job.company}</p>
+                  <p>{job.dateRange}</p>
+                </div>
+                <p className="text-white/80">{job.location}</p>
+                {job.description.map((paragraph) => {
+                  return (
+                    <p key={paragraph} className="py-3">
+                      {paragraph}
+                    </p>
+                  );
+                })}
               </div>
-              <p className="text-white/80">{job.location}</p>
-              <p className="py-6">{job.description}</p>
-            </div>
-          );
-        })}
+            );
+          })}
         </div>
       </div>
     </section>
