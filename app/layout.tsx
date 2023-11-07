03c22a0b654a03a7e5efc5986e15614a2745a775
Zack Hanni <zack.hannI@gmail.com>
2023-11-07 16:17:25 -0500

    remove unused components

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/* <Navbar /> */}
         {children}
-        {/* <Footer /> */}
       </body>
     </html>
   );
