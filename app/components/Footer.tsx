7ae96e4d216f251a645b4d84b58dccd7aea5009e
Zack Hanni <zack.hannI@gmail.com>
2023-11-07 16:15:33 -0500

    minimalize footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,72 +1,18 @@
-import {
-  FaRegEnvelope,
-  FaHome,
-  FaPhone,
-  FaRegFileAlt,
-} from "react-icons/fa";
 import Link from "next/link";
-import SocialData from "./SocialData";
+import React from "react";
+import SocialMedia from "./SocialMedia";
 
-function Footer() {
-  const ContactData = [
-    {
-      href: "/",
-      label: "Philadelphia, PA",
-      icon: <FaHome />,
-    },
-    {
-      href: "tel:8562983095",
-      label: "856-298-3095",
-      icon: <FaPhone />,
-    },
-    {
-      href: "mailto:zack.hanni@gmail.com",
-      label: "zack.hanni@gmail.com",
-      icon: <FaRegEnvelope />,
-    },
-    {
-      href: "https://drive.google.com/file/d/1XJAk8fHctsLu_8d6r15THYBmJV-uro1E/view?usp=sharing",
-      label: "Zack's Resume",
-      icon: <FaRegFileAlt />,
-    },
-  ];
-
+export default function Footer() {
   return (
-    <footer className="flex items-center justify-center  bg-slate-600 text-white py-20">
-      <div className="flex flex-col md:flex-row justify-between w-full max-w-[1100px] p-8 xl:p-0 ">
-        <div className="left-side pb-8 md:pb-0 text-lg">
-          <div className="title font-bold text-2xl py-2">Contact</div>
-
-          {ContactData.map((data) => {
-            return (
-              <Link href={data.href} key={data.label}>
-                <div className="flex">
-                  {data.icon}
-                  <p className="pl-2">{data.label}</p>
-                </div>
-              </Link>
-            );
-          })}
-        </div>
-        <div className="right-side text-lg">
-          <div className="title font-bold text-2xl py-2">About Me</div>
-          <div className="description max-w-xs">
-            I enjoy coding, learning, and audio engineering. I am open to
-            discussing new projects and design challenges.
+    <div className="flex flex-col items-center">
+      <SocialMedia />
+      <div>
+        <Link href="#hero">
+          <div className="text-black text-xl font-bold bg-amber-400 hover:bg-amber-500 py-3 px-6 rounded-md mx-8">
+            Back to top
           </div>
-          <div className="social-media flex ">
-            {SocialData.map((social) => {
-              return (
-                <Link href={social.href} key={social.label} target="_blank" >
-                  <div className="pr-4 pt-4 text-3xl">{social.icon}</div>
-                </Link>
-              );
-            })}
-          </div>
-        </div>
+        </Link>
       </div>
-    </footer>
+    </div>
   );
 }
-
-export default Footer;
