b0ea5a07be50c78f329e1e9e97e0caa10c94cfb7
Zack Hanni <zack.hannI@gmail.com>
2023-11-07 17:48:42 -0500

    increase social icon size and padding

--- app/components/SocialMedia.tsx
+++ app/components/SocialMedia.tsx
@@ -3,11 +3,11 @@ import SocialData from "./SocialData";
 
 export default function SocialMedia() {
   return (
-    <div className="social-media flex my-8 ">
+    <div className="social-media flex my-16 ">
     {SocialData.map((social) => {
       return (
         <Link key={social.label} href={social.href} target="_blank">
-          <div className="pr-4 text-3xl">{social.icon}</div>
+          <div className="pr-4 text-5xl">{social.icon}</div>
         </Link>
       );
     })}
