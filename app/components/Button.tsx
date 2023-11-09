4b1a61c0fdfb7e847c0bfd602178ff53bd4180d9
Zack Hanni <zack.hannI@gmail.com>
2023-11-09 11:24:02 -0500

    make dedicated button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+import { FaLock } from "react-icons/fa";
+
+interface ButtonProps {
+  name: string;
+  link: string;
+  password: boolean;
+}
+
+export default function Button(props: ButtonProps) {
+  return (
+    <Link href={props.link}>
+      <div className="text-black text-xl font-bold bg-amber-400 hover:bg-amber-500 py-3 px-6 rounded-md m-2 flex">
+        {props.name} {props.password ? <div className="pl-3"><FaLock/></div> : null }
+      </div>
+    </Link>
+  );
+}
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,18 +1,13 @@
 import Link from "next/link";
 import React from "react";
 import SocialMedia from "./SocialMedia";
+import Button from "./Button";
 
 export default function Footer() {
   return (
     <div className="flex flex-col items-center">
       <SocialMedia />
-      <div>
-        <Link href="#hero">
-          <div className="text-black text-xl font-bold bg-amber-400 hover:bg-amber-500 py-3 px-6 rounded-md mx-8">
-            Back to top
-          </div>
-        </Link>
-      </div>
+      <Button name="Back to top" password={false} link="#hero" />
     </div>
   );
 }
