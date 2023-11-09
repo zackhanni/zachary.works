27639019beb0887b6755318b2473ced9918d57cd
Zack Hanni <zack.hannI@gmail.com>
2023-11-09 16:46:47 -0500

    add website details in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,6 +8,9 @@ export default function Footer() {
     <div className="flex flex-col items-center">
       <SocialMedia />
       <Button name="Back to top" password={false} link="#hero" />
+      <p className="pt-16 max-w-md">
+        This website was designed in <b>Figma</b>, coded in <b>Visual Studio Code</b>, built with <b>Next.js</b>, and deployed with <b>Vercel</b> by yours truly.
+      </p>
     </div>
   );
 }
