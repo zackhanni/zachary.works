91d2edcba403ff465eebf5847b2c88609d0e7f7a
Zack Hanni <zack.hannI@gmail.com>
2023-11-09 16:49:58 -0500

    adjust spacing, wording, move tools into a component

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import "../components/ProjectsStyles.css";
 import { useInView } from "react-intersection-observer";
+import ToolBubbles from "./ToolBubbles";
 
 function Projects() {
   return (
@@ -12,10 +13,10 @@ function Projects() {
       id="projects"
       className="radial-gradient flex flex-col justify-center items-center py-8"
     >
-      <h2 className="text-5xl lg:text-7xl font-bold text-center py-4 text-white sticky top-0 z-10 w-full backdrop-blur">
-        Notable Projects
+      <h2 className="text-5xl font-bold text-center py-4 text-white sticky top-0 z-10 w-full backdrop-blur">
+        Projects
       </h2>
-      <ul className="max-w-[90%] md:max-w-[1200] m-auto grid gap-10 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 p-8 overflow-hidden">
+      <ul className="min-w-[50%] max-w-[90%] lg:max-w-[50%] m-auto grid gap-10 grid-cols-1 xl:grid-cols-2 py-8 overflow-hidden">
         {ProjectsData.map((project) => {
           // eslint-disable-next-line react-hooks/rules-of-hooks
           const { ref, inView } = useInView({
@@ -25,7 +26,7 @@ function Projects() {
 
           return (
             <li
-              className={`card md:max-w-[350px] min-w-[300px] bg-slate-400/10 backdrop-blur p-4 rounded-lg shadow-2xl ${
+              className={`card bg-slate-400/10 backdrop-blur p-4 rounded-lg shadow-2xl ${
                 inView ? "show" : "hide"
               }`}
               key={project.title}
@@ -41,28 +42,22 @@ function Projects() {
               <div className="py-3 text-white">
                 <div>
                   {/* <p>{inView.toString()}</p> */}
-                  <p className="text-2xl font-bold">{project.title}</p>
+                  <div className="flex justify-between items-center">
+                    <p className="text-lg font-bold">{project.title}</p>
                   <Link href={project.github}>
-                    <p className="text-blue-600 hover:text-blue-900">
-                      View on github
+                    <p className="text-blue-600 text-sm hover:text-blue-900">
+                      GitHub
                     </p>
                   </Link>
-                  <p className="py-6">{project.description}</p>
-                  <div className="flex flex-wrap pb-6">
-                    {project.tools?.map((tool) => {
-                      return (
-                        <p
-                          className="text-white bg-blue-800 rounded-full py-1 px-4 m-1"
-                          key={tool}
-                        >
-                          {tool}
-                        </p>
-                      );
-                    })}
                   </div>
+                  
+                  <p className="mt-1">{project.description}</p>
+
+                  <ToolBubbles tools={project.tools} />
+
                 </div>
                 <Link href={project.liveWebsite}>
-                  <button className="py-2 w-full bg-blue-800 hover:bg-blue-900 rounded-sm font-semibold">
+                  <button className="mt-4 py-2 w-full bg-blue-800 hover:bg-blue-900 rounded-sm font-semibold">
                     View Live Page
                   </button>
                 </Link>
