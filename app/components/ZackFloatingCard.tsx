9ddff277e18c10d18af1227d721d3993d56b227f
Zack Hanni <zack.hannI@gmail.com>
2023-11-27 11:58:53 -0500

    add tilt effect to ZackFloatingCard

--- app/components/ZackFloatingCard.tsx
+++ app/components/ZackFloatingCard.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import React from "react";
 import SocialMedia from "./SocialMedia";
+import { Tilt } from "react-tilt";
 
 export default function ZackFLoatingCard() {
   return (
-    <div className="hidden lg:flex flex-col sticky top-24 w-1/2 h-fit justify-start text-white rounded-lg my-6 mr-6 p-8">
+    <Tilt className="hidden lg:flex flex-col sticky top-24 w-1/2 h-fit justify-start text-white rounded-lg my-6 mr-6 p-8">
       <h2 className="text-5xl font-bold">Zack Hanni</h2>
       <h3 className="mt-3 text-xl font-medium">
         Experienced Software Engineer
@@ -12,6 +15,6 @@ export default function ZackFLoatingCard() {
         I build exceptional and accessible digital experiences for the web.
       </p>
       <SocialMedia />
-    </div>
+    </Tilt>
   );
 }
