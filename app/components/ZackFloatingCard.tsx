7ce179977af98255d8eac2a49269a9dcf6c5861d
Zack Hanni <zack.hannI@gmail.com>
2023-11-27 10:57:24 -0500

    make zack card only visible on large screens

--- app/components/ZackFloatingCard.tsx
+++ app/components/ZackFloatingCard.tsx
@@ -3,7 +3,7 @@ import SocialMedia from "./SocialMedia";
 
 export default function ZackFLoatingCard() {
   return (
-    <div className="hidden md:flex flex-col sticky top-24 w-1/2 h-fit justify-start text-white rounded-lg mt-6 mr-6 p-8 pb-0 mb-0">
+    <div className="hidden lg:flex flex-col sticky top-24 w-1/2 h-fit justify-start text-white rounded-lg my-6 mr-6 p-8">
       <h2 className="text-5xl font-bold">Zack Hanni</h2>
       <h3 className="mt-3 text-xl font-medium">
         Experienced Software Engineer
