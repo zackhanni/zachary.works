4b20edc544bf6a9420cd1697eb35528e476e513c
Zack Hanni <zack.hannI@gmail.com>
2023-11-27 09:44:05 -0500

    remove bg, adjust spacing of zack card

--- app/components/ZackFloatingCard.tsx
+++ app/components/ZackFloatingCard.tsx
@@ -3,7 +3,7 @@ import SocialMedia from "./SocialMedia";
 
 export default function ZackFLoatingCard() {
   return (
-    <div className="hidden sm:flex flex-col sticky top-0 w-1/2 justify-between py-24 radial-gradient text-white rounded-lg">
+    <div className="hidden md:flex flex-col sticky top-24 w-1/2 h-fit justify-start text-white rounded-lg mt-6 mr-6 p-8 pb-0 mb-0">
       <h2 className="text-5xl font-bold">Zack Hanni</h2>
       <h3 className="mt-3 text-xl font-medium">
         Experienced Software Engineer
