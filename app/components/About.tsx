abdd326f04c1d35aa9835c107a21441d0dccd0e2
Zack Hanni <zack.hannI@gmail.com>
2023-11-27 09:45:47 -0500

    add ZackFloatingCard

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,6 +1,7 @@
 import { FaRegEnvelope, FaHome, FaPhone, FaRegFileAlt } from "react-icons/fa";
 import Link from "next/link";
 import Footer from "./Footer";
+import ZackFLoatingCard from "./ZackFloatingCard";
 
 export default function About() {
   const TechStack = [
@@ -29,89 +30,94 @@ export default function About() {
   return (
     <section className="">
       <div className="bg-black text-white py-28 flex flex-col items-center justify-center">
-        <div className="min-w-[50%] max-w-[90%] lg:max-w-[50%] md:max-w-[75%]">
-          {/* About me */}
-          <div
-            className="radial-gradient p-8 
+        <div className="flex max-w-[90%] xl:max-w-[80%] 2xl:max-w-[70%]">
+          <ZackFLoatingCard />
+          <div id="right-side min-w-[50%] max-w-[90%] sm:max-w-[75%] md:max-w-[50%]">
+            {/* About me */}
+            <div
+              className="radial-gradient p-8 
         mt-6 rounded-lg backdrop-blur"
-          >
-            <p className="text-3xl font-bold">About Me</p>
-            <p className="py-6">
-              Dedicated, Analytical, and Motivated Software Engineering
-              Professional with expertise in web development and optimization.
-              Leverages technical expertise to deliver information technology
-              solutions.
-            </p>
-          </div>{" "}
-          {/* tech i love */}
-          <div
-            className="radial-gradient p-8 
+            >
+              <p className="text-3xl font-bold">More About Me</p>
+              <p className="py-6">
+                Dedicated, Analytical, and Motivated Software Engineering
+                Professional with expertise in web development and optimization.
+                Leverages technical expertise to deliver information technology
+                solutions.
+              </p>
+            </div>{" "}
+            {/* tech i love */}
+            <div
+              className="radial-gradient p-8 
         mt-6 rounded-lg backdrop-blur"
-          >
-            <p className="text-3xl font-bold">Tech I Love</p>
-            <p className="py-6">
-              My choice tech stack for personal projects is GENERALLY (but not
-              always):
-            </p>
-            <div className="grid gap-2 grid-cols-1 md:grid-cols-2">
-              {TechStack.map((tech) => {
-                return <li key={tech}>{tech}</li>;
-              })}
-            </div>
+            >
+              <p className="text-3xl font-bold">Tech I Love</p>
+              <p className="py-6">
+                My choice tech stack for personal projects is GENERALLY (but not
+                always):
+              </p>
+              <div className="grid gap-2 grid-cols-1 md:grid-cols-2">
+                {TechStack.map((tech) => {
+                  return <li key={tech}>{tech}</li>;
+                })}
+              </div>
 
-            <p className="py-6">
-              Some other tech that I love and/or have built at scale with:
-            </p>
-            <div className="grid gap-2 grid-cols-1 md:grid-cols-2">
-              {AltStack.map((tech) => {
-                return <li key={tech}>{tech}</li>;
-              })}
+              <p className="py-6">
+                Some other tech that I love and/or have built at scale with:
+              </p>
+              <div className="grid gap-2 grid-cols-1 md:grid-cols-2">
+                {AltStack.map((tech) => {
+                  return <li key={tech}>{tech}</li>;
+                })}
+              </div>
             </div>
-          </div>
-          {/* contact box */}
-          <div
-            className="radial-gradient p-8 
+            {/* contact box */}
+            <div
+              className="radial-gradient p-8 
         mt-6 rounded-lg backdrop-blur "
-          >
-            <h2 id="contact" className="text-3xl font-bold">Contact</h2>
-            <p className="py-6">
-              The best way to contact me is through email or LinkedIn (I&apos;m
-              not much of a twitter guy). Give me a shout here:
-            </p>
-            <div>
-              <p>
-                Email:
-                <a
-                  href="mailto:zack.hanni@gmail.com"
-                  target="blank"
-                  className="hover:underline hover:text-blue-700"
-                >
-                  zack.hanni@gmail.com
-                </a>
+            >
+              <h2 id="contact" className="text-3xl font-bold">
+                Contact
+              </h2>
+              <p className="py-6">
+                The best way to contact me is through email or LinkedIn
+                (I&apos;m not much of a twitter guy). Give me a shout here:
               </p>
-              <p>
-                LinkedIn:
-                <a
-                  href="https://www.linkedin.com/in/zackhanni/"
-                  target="blank"
-                  className="hover:underline hover:text-blue-700"
-                >
-                  @zackhanni
-                </a>
-              </p>
-              <p>
-                <a
-                  href="https://drive.google.com/file/d/1XJAk8fHctsLu_8d6r15THYBmJV-uro1E/view?usp=sharing"
-                  target="blank"
-                  className="hover:underline hover:text-blue-700"
-                >
-                  Zack&apos;s Resume
-                </a>
-              </p>
+              <div>
+                <p>
+                  Email:
+                  <a
+                    href="mailto:zack.hanni@gmail.com"
+                    target="blank"
+                    className="hover:underline hover:text-blue-700"
+                  >
+                    zack.hanni@gmail.com
+                  </a>
+                </p>
+                <p>
+                  LinkedIn:
+                  <a
+                    href="https://www.linkedin.com/in/zackhanni/"
+                    target="blank"
+                    className="hover:underline hover:text-blue-700"
+                  >
+                    @zackhanni
+                  </a>
+                </p>
+                <p>
+                  <a
+                    href="https://drive.google.com/file/d/1XJAk8fHctsLu_8d6r15THYBmJV-uro1E/view?usp=sharing"
+                    target="blank"
+                    className="hover:underline hover:text-blue-700"
+                  >
+                    Zack&apos;s Resume
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
-          <Footer />
         </div>
+        <Footer />
       </div>
     </section>
   );
--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import WorkData from "./ExperienceData";
 import ToolBubbles from "./ToolBubbles";
+import ZackFLoatingCard from "./ZackFloatingCard";
 
 export default function Experience() {
   return (
@@ -12,31 +13,34 @@ export default function Experience() {
         >
           Experience
         </h2>
-        <div className="min-w-[50%] max-w-[90%] lg:max-w-[50%] md:max-w-[75%]">
-          {WorkData.map((job) => {
-            return (
-              <div
-                key={job.company}
-                className="bg-slate-800/20 p-8 
+        <div className="flex max-w-[90%] xl:max-w-[80%] 2xl:max-w-[70%] ">
+          <ZackFLoatingCard />
+          <div className="min-w-[50%] max-w-[90%] sm:max-w-[75%] md:max-w-[50%]">
+            {WorkData.map((job) => {
+              return (
+                <div
+                  key={job.company}
+                  className="bg-slate-800/20 p-8 
         mt-6 rounded-lg radial-gradient backdrop-blur"
-              >
-                <p className="text-2xl font-semibold">{job.title}</p>
-                <div className="flex text-white/80">
-                  <p className="text-[#514E6D] mr-1">{job.company}</p>
-                  <p>{job.dateRange}</p>
+                >
+                  <p className="text-2xl font-semibold">{job.title}</p>
+                  <div className="flex text-white/80">
+                    <p className="text-[#514E6D] mr-1">{job.company}</p>
+                    <p>{job.dateRange}</p>
+                  </div>
+                  <p className="text-white/80">{job.location}</p>
+                  {job.description.map((paragraph) => {
+                    return (
+                      <p key={paragraph} className="py-3">
+                        {paragraph}
+                      </p>
+                    );
+                  })}
+                  <ToolBubbles tools={job.tools} />
                 </div>
-                <p className="text-white/80">{job.location}</p>
-                {job.description.map((paragraph) => {
-                  return (
-                    <p key={paragraph} className="py-3">
-                      {paragraph}
-                    </p>
-                  );
-                })}
-                <ToolBubbles tools={job.tools} />
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
