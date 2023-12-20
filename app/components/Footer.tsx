f93bcc7e010ead78427ba5eb8f3806af2803d378
Zack Hanni <zack.hannI@gmail.com>
2023-12-20 09:28:44 -0500

    potential button. hidden

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,6 +7,11 @@ export default function Footer() {
   return (
     <div className="flex flex-col items-center">
       <SocialMedia />
+      {/* <Button
+        name="Resume"
+        password={false}
+        link="https://drive.google.com/file/d/1XJAk8fHctsLu_8d6r15THYBmJV-uro1E/view?usp=sharing"
+      /> */}
       <Button name="Back to top" password={false} link="#hero" />
       <p className="pt-16 max-w-md">
         This website was designed in{" "}
