e98d211499fa14756698af121d74e0a879036c62
Zack Hanni <zack.hannI@gmail.com>
2023-12-24 18:26:14 -0500

    prettier

--- app/components/ToolBubbles.tsx
+++ app/components/ToolBubbles.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 
 interface ToolBubblesProps {
-    tools?: string[];
-  }
+  tools?: string[];
+}
 
 export default function ToolBubbles(props: ToolBubblesProps) {
   return (
     <div className="flex flex-wrap">
-    {props.tools?.map((tool: string) => {
-      return (
-        <p
-          className="text-white text-sm bg-[#514E6D] font-medium rounded-full py-1 px-3 mr-1.5 mt-2"
-          key={tool}
-        >
-          {tool}
-        </p>
-      );
-    })}
-  </div>
-  )
+      {props.tools?.map((tool: string) => {
+        return (
+          <p
+            className="text-white text-sm bg-[#514E6D] font-medium rounded-full py-1 px-3 mr-1.5 mt-2"
+            key={tool}
+          >
+            {tool}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
