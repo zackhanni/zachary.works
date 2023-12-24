d7bdd68303cb9b447bb16e828d67ffddbe8a5eca
Zack Hanni <zack.hannI@gmail.com>
2023-12-24 18:26:40 -0500

    decrease icon size

--- app/components/SocialMedia.tsx
+++ app/components/SocialMedia.tsx
@@ -4,13 +4,15 @@ import SocialData from "./SocialData";
 export default function SocialMedia() {
   return (
     <div className="social-media flex my-16 ">
-    {SocialData.map((social) => {
-      return (
-        <Link key={social.label} href={social.href} target="_blank">
-          <div className="pr-4 text-5xl hover:text-[#514E6D] duration-300">{social.icon}</div>
-        </Link>
-      );
-    })}
-  </div>
-  )
+      {SocialData.map((social) => {
+        return (
+          <Link key={social.label} href={social.href} target="_blank">
+            <div className="pr-4 text-4xl hover:text-[#514E6D] duration-300">
+              {social.icon}
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
