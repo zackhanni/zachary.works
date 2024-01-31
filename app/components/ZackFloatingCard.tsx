d9a25b22edacb458b28238a015f9a508ed4285aa
Zack Hanni <zack.hannI@gmail.com>
2024-01-30 23:47:29 -0500

    added a bg color to the floating card, adjusted margin

--- app/components/ZackFloatingCard.tsx
+++ app/components/ZackFloatingCard.tsx
@@ -6,7 +6,7 @@ import { Tilt } from "react-tilt";
 
 export default function ZackFLoatingCard() {
   return (
-    <Tilt className="hidden lg:flex flex-col sticky top-24 w-1/2 h-fit justify-start text-white rounded-lg my-6 mr-6 p-8">
+    <Tilt className="hidden lg:flex flex-col sticky top-24 w-1/2 h-fit justify-start text-white rounded-lg mt-6 mr-6 p-8 bg-zinc-950">
       <h2 className="text-3xl font-bold">Zack Hanni</h2>
       <h3 className="font-medium">Experienced Software Engineer</h3>
       <p className="my-4 max-w-xs text-sm">
