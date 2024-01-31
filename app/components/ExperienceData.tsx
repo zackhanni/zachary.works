95ebccad8e7422fcc8e9e709b880d8933a849d5c
Zack Hanni <zack.hannI@gmail.com>
2024-01-30 19:13:17 -0500

    updated work experience

--- app/components/ExperienceData.tsx
+++ app/components/ExperienceData.tsx
@@ -6,10 +6,10 @@ const ExperienceData = [
     location: "Philadelphia, Pennsylvania / Hybrid",
     tools: ["Next.js", "Tailwind", "MongoDB", "WordPress", "TypeScript", "PHP"],
     description: [
-      "Create websites, interactive games, and digital media for nationally recognized brands and industries.",
-      "Oversee and develop numerous websites and web applications from concept to completion.",
-      "Deliver products utilizing content management systems such as WordPress.",
-      "Maintain HTML communication emails, newsletters, email campaigns, and online advertisements for customer engagement programs.",
+      "Develop pixel-perfect websites and interactive web-based game experiences, catering to the distinctive needs of nationally recognized brands.",
+      "Offer continuous support and maintenance for websites built with WordPress and React.js.",
+      "Manage and optimize HTML newsletters, email campaigns, and advertisements as integral components of customer engagement programs, fostering enhanced brand interactions.",
+      "Collaborate with project managers, designers, content writers, and clients, maintaining a professional, clear, and friendly communication style to ensure project success and client satisfaction.",
     ],
   },
   {
@@ -19,20 +19,10 @@ const ExperienceData = [
     location: "Philadelphia, Pennsylvania",
     tools: ["Ruby", "Jekyll", "Bootstrap", "SCSS", "Netlify"],
     description: [
-      "I designed, built, and deployed responsive, service-based web pages for a Philadelphia-based recording studio. Every page was meticulously optimized for SEO keywords, swift load speeds, and enhanced search rankings on search engines",
-      "Throughout this process, I have successfully elevated online engagement and studio bookings by over 300%, secured numerous top 10 search rankings on Google, and streamlined it's operational efficiency.",
-      "Furthermore, I have undertaken the responsibility of overseeing all aspects of the studio's technical infrastructure. This includes the maintenance of computers, management of network attached storage, daily system backups, and technical support for the studio's four active recording spaces.",
-    ],
-  },
-  {
-    title: "Software Engineer",
-    company: "Freelance",
-    dateRange: "• January 2017 - August 2023",
-    tools: ["React", "JavaScript", "CSS", "HTML", "WordPress"],
-    description: [
-      "My experience as a freelance software engineer has equipped me with the ability to navigate diverse client requirements and constraints effectively - From developing solutions for schoolteachers aiming to enhance online student engagement to assisting visually impaired podcasters in conveying their experiences to their audience.",
-      "In addition to my freelance endeavors, my proximity to the music industry has given me the opportunity to collaborate closely with local musicians. In this capacity, I have played a pivotal role in the creation of promotional content, electronic press kits, album artwork, and other audiovisual materials, thereby facilitating the growth of artists' careers. My expertise in crafting concise, single-page websites has allowed me to optimize my workflow, delivering high-quality results swiftly and cost-effectively, all while maintaining a polished and professional appearance.",
-      "For further information on specific freelance projects,  descriptions and demos are provided below.",
+      "Conceptualized, designed, developed, and deployed cutting-edge responsive websites and interactive promotional material tailored for recording studios, music labels, and independent artists.",
+      "Spearheaded successful implementation of SEO best practices, resulting in a substantial enhancement of search rankings and consecutive yearly sales growth for five years.",
+      "Optimized upload and download speeds to the local server, resulting in significant performance improvements across 4 recording studio spaces.",
+      "Delivered comprehensive IT technical support and maintenance services for all computers, servers, and a vast repository of over 50TB of digital assets.",
     ],
   },
 ];
