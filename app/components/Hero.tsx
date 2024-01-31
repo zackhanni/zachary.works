f7886c2720b7b07bc4c3eab46f7e439148f3d3c0
Zack Hanni <zack.hannI@gmail.com>
2024-01-30 23:52:48 -0500

    fixed overscrol background, decreased text size

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,8 +5,8 @@ import { FaChevronDown } from "react-icons/fa";
 
 function Hero() {
   return (
-    <section id="hero" className="h-screen w-full relative bg-black -z-10">
-      <div className="fixed h-full w-full left-0 right-0">
+    <section id="hero" className="h-screen w-full relative -z-10">
+      <div className="fixed h-full w-full left-0 right-0 bg-black">
         <Image
           src="/hero-image-table.jpg"
           fill={true}
@@ -15,19 +15,18 @@ function Hero() {
           style={{}}
           className="opacity-20 object-cover w-screen"
         />
-        <div className="max-md:p-4 p-0 absolute md:-translate-x-1/2 -translate-y-1/2 top-1/2 md:left-1/2 text-white max-w-[1200]">
-          <p className="text-5xl lg:text-7xl font-bold">Hi, I&apos;m Zack.</p>
-          {/* <SocialMedia /> */}
-          <p className="text-xl pt-8">
+        <div className="px-8 sm:px-16 lg:px-0 absolute md:-translate-x-1/2 -translate-y-1/2 top-1/2 md:left-1/2 text-white max-w-[1200]">
+          <p className="text-4xl font-bold">Hi, I&apos;m Zack.</p>
+          <p className="pt-4">
             I&apos;m an audio engineer, turned{" "}
-            <span id="rainbow" className="font-semibold text-xl">
+            <span id="rainbow" className="font-semibold text-lg">
               software engineer
             </span>
             , based in Philadelphia, PA. This website is my personal portfolio,
             highlighting my work history, notable projects, and my profound love
             of tech.
           </p>
-          <div className="text-8xl grid place-items-center pt-32">
+          <div className="text-4xl grid place-items-center pt-32">
             <FaChevronDown className="pulse" />
           </div>
 
