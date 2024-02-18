214c6e3c9d0e2f8b3b1727a1f28ef7b6e67c344a
Zack Hanni <zack.hannI@gmail.com>
2024-02-18 16:08:02 -0500

    updated mood board image, hid smaller projects

--- app/components/ProjectsData.tsx
+++ app/components/ProjectsData.tsx
@@ -1,16 +1,6 @@
 const ProjectsData = [
   {
-    src: "/soma-website.png",
-    tools: ["Ruby", "Jekyll", "Bootstrap", "Netlify"],
-    title: "Sounds Like Soma",
-    company: "Sounds Like Soma",
-    description:
-      "I fully designed, developed  and deployed a responsive website for a local recording studio, incorporating user experience research, and SEO best practices.",
-    liveWebsite: "https://www.soundslikesoma.com/",
-    github: "https://github.com/zackhanni/soundslikesoma",
-  },
-  {
-    src: "/emo-intel.png",
+    src: "/mood-board.png",
     tools: [
       "React",
       "Next.js",
@@ -23,9 +13,19 @@ const ProjectsData = [
     ],
     title: "Mood Board",
     description:
-      "A mental health-focused application designed to educate users on their emotional well-being, and track changes in mood.",
+      "A fully functional mental health-focused application designed to educate users on their emotional well-being, and track changes in mood.",
     liveWebsite: "https://mood-board-next.vercel.app/",
     github: "https://github.com/zackhanni/how-are-you-feeling",
+  },
+  {
+    src: "/soma-website.png",
+    tools: ["Ruby", "Jekyll", "Bootstrap", "Netlify"],
+    title: "Sounds Like Soma",
+    company: "Sounds Like Soma",
+    description:
+      "I fully designed, developed  and deployed a responsive website for a local recording studio, incorporating user experience research, and SEO best practices.",
+    liveWebsite: "https://www.soundslikesoma.com/",
+    github: "https://github.com/zackhanni/soundslikesoma",
   },
   {
     src: "/visual-impairment.png",
@@ -56,22 +56,21 @@ const ProjectsData = [
     liveWebsite: "https://dribbble.com/zackhanni",
     github: "https://dribbble.com/zackhanni",
   },
-
-  {
-    src: "/dino.jpg",
-    title: "THE-saurus",
-    description: "A dinosaur themed thesaurus utilizing Dictionary.com's API",
-    liveWebsite: "https://the-saurus.vercel.app/",
-    github: "https://github.com/zackhanni/the-saurus",
-  },
-  {
-    src: "/weather.jpg",
-    title: "Weather search app",
-    description:
-      "I accessed a weather API to find temperature highs, lows, humidity, etc in any city around the world. This app is deployed through Vercel",
-    liveWebsite: "https://next-weather-app-zackhanni.vercel.app/",
-    github: "https://github.com/zackhanni/weather-app-react",
-  },
+  // {
+  //   src: "/dino.jpg",
+  //   title: "THE-saurus",
+  //   description: "A dinosaur themed thesaurus utilizing Dictionary.com's API",
+  //   liveWebsite: "https://the-saurus.vercel.app/",
+  //   github: "https://github.com/zackhanni/the-saurus",
+  // },
+  // {
+  //   src: "/weather.jpg",
+  //   title: "Weather search app",
+  //   description:
+  //     "I accessed a weather API to find temperature highs, lows, humidity, etc in any city around the world. This app is deployed through Vercel",
+  //   liveWebsite: "https://next-weather-app-zackhanni.vercel.app/",
+  //   github: "https://github.com/zackhanni/weather-app-react",
+  // },
 ];
 
 export default ProjectsData;
