37a591b0953e5d772a566ef0aa73cf834b385aa9
Zack Hanni <zack.hannI@gmail.com>
2024-03-12 12:19:15 -0400

    adjust padding and width on hero description text

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ import SocialMedia from "./SocialMedia";
 import Button from "./Button";
 import { FaChevronDown } from "react-icons/fa";
 
-function Hero() {
+export default function Hero() {
   return (
     <section id="hero" className="h-screen w-full relative -z-10">
       <div className="fixed h-full w-full left-0 right-0 bg-black">
@@ -15,7 +15,7 @@ function Hero() {
           style={{}}
           className="opacity-20 object-cover w-screen"
         />
-        <div className="px-8 sm:px-16 lg:px-0 absolute md:-translate-x-1/2 -translate-y-1/2 top-1/2 md:left-1/2 text-white max-w-[1200]">
+        <div className="px-8 md:px-0 absolute md:-translate-x-1/2 -translate-y-1/2 top-1/2 md:left-1/2 text-white">
           <p className="text-4xl font-bold">Hi, I&apos;m Zack.</p>
           <p className="pt-4 text-lg">
             I&apos;m an audio engineer, turned{" "}
@@ -26,19 +26,16 @@ function Hero() {
             highlighting my work history, notable projects, and my profound love
             of tech.
           </p>
-          <div className="text-4xl grid place-items-center pt-32">
-            <FaChevronDown className="pulse" />
-          </div>
-
-          {/* <div className="z-50">
-            <Button name="Work Experience" password={false} link="#work" />
+          {/* <div className="flex z-50 ">
+            <Button name="Experience" password={false} link="#experience" />
             <Button name="Projects" password={false} link="#projects" />
             <Button name="Contact" password={false} link="#contact" />
           </div> */}
+          <div className="text-4xl grid place-items-center pt-32">
+            <FaChevronDown className="pulse" />
+          </div>
         </div>
       </div>
     </section>
   );
 }
-
-export default Hero;
