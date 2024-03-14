c2a8e3a32a637aa15889057a392fc5395de46d34
Zack Hanni <zack.hannI@gmail.com>
2024-03-13 23:11:06 -0400

    added hero navigation buttons. they autohide after scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
+import HeroButtons from "./HeroButtons";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <HeroButtons />
       <Experience />
       <Projects />
       <About />
