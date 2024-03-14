2e90107cff9a07fa0c49fe48af2019ebeb0c7ee7
Zack Hanni <zack.hannI@gmail.com>
2024-03-14 00:44:40 -0400

    updated floating card spacing and padding

--- app/components/ZackFloatingCard.tsx
+++ app/components/ZackFloatingCard.tsx
@@ -6,13 +6,19 @@ import { Tilt } from "react-tilt";
 
 export default function ZackFLoatingCard() {
   return (
-    <Tilt className="hidden lg:flex flex-col sticky top-24 w-1/2 h-fit justify-start text-white rounded-lg mt-6 mr-6 p-8 bg-zinc-950">
+    <Tilt
+      className="hidden lg:flex flex-col sticky top-24 w-1/2 h-fit justify-start text-white rounded-lg mr-4 p-4
+     bg-blue-500 z-10
+    "
+    >
       <h2 className="text-4xl font-bold">Zack Hanni</h2>
-      <h3 className="text-xl">Experienced Software Engineer</h3>
-      <p className="my-4 max-w-xs">
+      <h3 className="text-xl font-semibold">Experienced Software Engineer</h3>
+      <p className="my-4">
         I build exceptional and accessible digital experiences for the web.
       </p>
       <SocialMedia />
     </Tilt>
   );
 }
+
+// bg-zinc-950
