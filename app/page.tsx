89ca979b193d0bfc67099b6bd08295b1a71262cb
Zack Hanni <zack.hannI@gmail.com>
2024-03-15 01:32:30 -0400

    fixed import link for hero buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
-import HeroButtons from "./HeroButtons";
+import HeroButtons from "./components/HeroButtons";
 
 export default function Home() {
   return (
