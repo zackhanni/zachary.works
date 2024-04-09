fc79199c8c95d0eaafd38ab54f5bf2fcb7f1a188
Zack Hanni <zack.hannI@gmail.com>
2024-04-09 00:44:53 -0400

    Update Projects.tsx

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -13,58 +13,60 @@ function Projects() {
       id="projects"
       className="radial-gradient flex flex-col justify-center items-center pb-16"
     >
-      <h2 className="text-4xl font-bold text-center py-4 text-white sticky top-0 z-10 w-full backdrop-blur">
-        Projects
-      </h2>
+      <div className="">
+        <h2 className="text-4xl font-bold text-center py-4 text-white sticky top-0 z-10 w-full backdrop-blur">
+          Projects
+        </h2>
 
-      <ul className="card-container m-auto grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 overflow-hidden">
-        {ProjectsData.map((project) => {
-          // eslint-disable-next-line react-hooks/rules-of-hooks
-          const { ref, inView } = useInView({
-            threshold: 0.15,
-            triggerOnce: true,
-          });
+        <ul className="card-container m-auto grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 overflow-hidden">
+          {ProjectsData.map((project) => {
+            // eslint-disable-next-line react-hooks/rules-of-hooks
+            const { ref, inView } = useInView({
+              threshold: 0.15,
+              triggerOnce: true,
+            });
 
-          return (
-            <li
-              className={`card bg-slate-400/10 sm:w-[300px] backdrop-blur p-4 rounded-lg shadow-2xl ${
-                inView ? "show" : "hide"
-              }`}
-              key={project.title}
-              ref={ref}
-            >
-              <div className="overflow-hidden">
-                <Link href={project.liveWebsite} target="_blank">
-                  <Image
-                    src={project.src}
-                    height={175}
-                    width={350}
-                    alt={project.title}
-                    className="object-cover w-full h-[175px] rounded-md hover:scale-110 hover:grayscale ease-out duration-500"
-                  />
-                </Link>
-              </div>
-              <div className="py-3 text-white text-sm">
-                <div>
-                  {/* <p>{inView.toString()}</p> */}
-                  <div className="flex justify-between items-center">
-                    <p className="font-bold text-lg">{project.title}</p>
-                    {/* <Link href={project.github} target="_blank">
+            return (
+              <li
+                className={`card bg-slate-400/10 sm:w-[300px] backdrop-blur p-4 rounded-lg shadow-2xl ${
+                  inView ? "show" : "hide"
+                }`}
+                key={project.title}
+                ref={ref}
+              >
+                <div className="overflow-hidden">
+                  <Link href={project.liveWebsite} target="_blank">
+                    <Image
+                      src={project.src}
+                      height={175}
+                      width={350}
+                      alt={project.title}
+                      className="object-cover w-full h-[175px] rounded-md hover:scale-110 hover:grayscale ease-out duration-500"
+                    />
+                  </Link>
+                </div>
+                <div className="py-3 text-white text-sm">
+                  <div>
+                    {/* <p>{inView.toString()}</p> */}
+                    <div className="flex justify-between items-center">
+                      <p className="font-bold text-lg">{project.title}</p>
+                      {/* <Link href={project.github} target="_blank">
                       <p className="text-[#514E6D] text-sm hover:text-white duration-300">
                         GitHub
                       </p>
                     </Link> */}
+                    </div>
+
+                    <p className="mt-1 text-sm">{project.description}</p>
+
+                    <ToolBubbles tools={project.tools} />
                   </div>
-
-                  <p className="mt-1 text-sm">{project.description}</p>
-
-                  <ToolBubbles tools={project.tools} />
                 </div>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </section>
   );
 }
