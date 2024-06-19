01d6f2d3660112257da71fb099c4065109ce6e60
Zack Hanni <zack.hannI@gmail.com>
2024-06-19 15:01:56 -0400

    updated job tools

--- app/components/ExperienceData.tsx
+++ app/components/ExperienceData.tsx
@@ -1,10 +1,17 @@
 const ExperienceData = [
   {
     title: "Web Developer & Digital Solutions Lead",
-    company: "",
+    company: "SMC",
     dateRange: "• August 2023 - Now",
     location: "Philadelphia, Pennsylvania / Hybrid",
-    tools: ["Next.js", "Tailwind", "MongoDB", "WordPress", "TypeScript", "PHP"],
+    tools: [
+      "Next.js",
+      "Tailwind",
+      "React",
+      "React Native",
+      "WordPress",
+      "TypeScript",
+    ],
     description: [
       "Develop pixel-perfect websites and interactive web-based game experiences, catering to the distinctive needs of nationally recognized brands.",
       "Offer continuous support and maintenance for websites built with WordPress and React.js.",
@@ -17,7 +24,14 @@ const ExperienceData = [
     company: "Sounds Like Soma",
     dateRange: "• January 2018 - August 2023",
     location: "Philadelphia, Pennsylvania",
-    tools: ["Ruby", "Jekyll", "Bootstrap", "SCSS", "Netlify"],
+    tools: [
+      "React",
+      "Next.js",
+      "Bootstrap",
+      "RESTful APIs",
+      "Tailwind",
+      "Typescript",
+    ],
     description: [
       "Conceptualized, designed, developed, and deployed cutting-edge responsive websites and interactive promotional material tailored for recording studios, music labels, and independent artists.",
       "Spearheaded successful implementation of SEO best practices, resulting in a substantial enhancement of search rankings and consecutive yearly sales growth for five years.",
