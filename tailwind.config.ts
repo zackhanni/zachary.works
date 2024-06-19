67f3b0fba45de363b920b1073e7a36ba071f074e
Zack Hanni <zack.hannI@gmail.com>
2024-06-19 17:47:25 -0400

    formatted with prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,51 +1,51 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
     screens: {
-      'sm': '640px',
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
 
-      'max-2xl': {'max': '1535px'},
+      "max-2xl": { max: "1535px" },
       // => @media (max-width: 1535px) { ... }
 
-      'max-xl': {'max': '1279px'},
+      "max-xl": { max: "1279px" },
       // => @media (max-width: 1279px) { ... }
 
-      'max-lg': {'max': '1023px'},
+      "max-lg": { max: "1023px" },
       // => @media (max-width: 1023px) { ... }
 
-      'max-md': {'max': '767px'},
+      "max-md": { max: "767px" },
       // => @media (max-width: 767px) { ... }
 
-      'max-sm': {'max': '639px'},
+      "max-sm": { max: "639px" },
       // => @media (max-width: 639px) { ... }
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
