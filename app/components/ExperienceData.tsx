c6720b3f024cbc3870c79d90eac64d33dfb27c84
Zack Hanni <zack.hannI@gmail.com>
2024-06-20 12:04:27 -0400

    Update ExperienceData.tsx

--- app/components/ExperienceData.tsx
+++ app/components/ExperienceData.tsx
@@ -13,11 +13,41 @@ const ExperienceData = [
       "TypeScript",
     ],
     description: [
-      "Develop pixel-perfect websites and interactive web-based game experiences, catering to the distinctive needs of nationally recognized brands.",
-      "Offer continuous support and maintenance for websites built with WordPress and React.js.",
-      "Manage and optimize HTML newsletters, email campaigns, and advertisements as integral components of customer engagement programs, fostering enhanced brand interactions.",
-      "Collaborate with project managers, designers, content writers, and clients, maintaining a professional, clear, and friendly communication style to ensure project success and client satisfaction.",
+      "At SMC, I contributed to the digital success of nationally recognized brands by delivering high-quality web solutions. My key responsibilities included:",
     ],
+    bullets: (
+      <ul className="space-y-4 list-disc">
+        <li>
+          <span className="font-medium text-slate-200">Web Development</span>:
+          Developed pixel-perfect websites and interactive web experiences,
+          meticulously catering to the distinctive needs of nationally
+          recognized brands.
+        </li>
+        <li>
+          <span className="font-medium text-slate-200">
+            Mobile Game Development
+          </span>
+          : Created interactive Android and iOS games using React Native,
+          collaborating with large corporations and nonprofits to deliver
+          engaging mobile experiences.
+        </li>
+        <li>
+          <span className="font-medium text-slate-200">
+            Support and Maintenance
+          </span>
+          : Provided continuous support and maintenance for websites built with
+          WordPress and React.js, ensuring optimal performance and reliability.
+        </li>
+        <li>
+          <span className="font-medium text-slate-200">
+            Collaborative Communication
+          </span>
+          : Worked closely with project managers, designers, content writers,
+          and clients, maintaining a professional, clear, and friendly
+          communication style to ensure project success and client satisfaction.
+        </li>
+      </ul>
+    ),
   },
   {
     title: "Software Engineer",
@@ -32,12 +62,42 @@ const ExperienceData = [
       "Tailwind CSS",
       "Typescript",
     ],
+
     description: [
-      "Conceptualized, designed, developed, and deployed cutting-edge responsive websites and interactive promotional material tailored for recording studios, music labels, and independent artists.",
-      "Spearheaded successful implementation of SEO best practices, resulting in a substantial enhancement of search rankings and consecutive yearly sales growth for five years.",
-      "Optimized upload and download speeds to the local server, resulting in significant performance improvements across 4 recording studio spaces.",
-      "Delivered comprehensive IT technical support and maintenance services for all computers, servers, and a vast repository of over 50TB of digital assets.",
+      "At Sounds Like Soma, I played a pivotal role in enhancing the digital presence and operational efficiency of recording studios, music labels, and independent artists. My key contributions included:",
     ],
+    bullets: (
+      <ul className="space-y-4 list-disc">
+        <li>
+          <span className="font-medium text-slate-200">Web Development</span>:
+          Conceptualized, designed, developed, and deployed cutting-edge
+          responsive websites and interactive promotional materials tailored
+          specifically for the music industry.
+        </li>
+        <li>
+          <span className="font-medium text-slate-200">SEO Implementation</span>
+          : Spearheaded successful implementation of SEO best practices,
+          significantly enhancing search rankings and driving consecutive yearly
+          sales growth over five years.
+        </li>
+        <li>
+          <span className="font-medium text-slate-200">
+            Performance Optimization
+          </span>
+          : Optimized upload and download speeds to the local server, resulting
+          in notable performance improvements across four recording studio
+          spaces.
+        </li>
+        <li>
+          <span className="font-medium text-slate-200">
+            IT Support and Maintenance
+          </span>
+          : Delivered comprehensive IT technical support and maintenance
+          services, managing all computers, servers, and a vast repository of
+          over 50TB of digital assets.
+        </li>
+      </ul>
+    ),
   },
 ];
 
