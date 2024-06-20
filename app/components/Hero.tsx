f8365edd4bdf42c1de2cd2dcb19f24c0d5d7ecc3
Zack Hanni <zack.hannI@gmail.com>
2024-06-20 12:04:52 -0400

    updated hero text content

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <section id="hero" className="h-screen w-full">
       <div className="relative -z-20 pointer-events-none">
-        <div className="fixed h-full w-full left-0 radial-gradient text-white">
+        <div className="fixed h-full w-full left-0 radial-gradient text-[#94A3B8]">
           {/* <Image
             src="/desk-with-computers.jpeg"
             fill={true}
@@ -17,8 +17,24 @@ export default function Hero() {
           /> */}
 
           <div className="px-4 md:px-0 absolute md:-translate-x-1/2 -translate-y-1/2 top-1/2 md:left-1/2 w-full max-w-2xl">
-            <p className="text-4xl font-bold">Hi, I&apos;m Zack.</p>
+            <p className="text-5xl font-bold text-slate-200">
+              Hi! I&apos;m Zack,
+            </p>
             <p className="text-lg">
+              a former audio engineer turned software engineer. With a rich
+              background in the music industry, working alongside artists and
+              music labels, I&apos;ve transitioned my creative problem-solving
+              skills into the world of technology.
+            </p>
+            <p className="text-lg">
+              Here, you&apos;ll find a showcase of my software engineering
+              projects, highlighting my expertise in web development, natural
+              language processing, and user-centric design. Explore my journey
+              from soundwaves to code, and see how my unique blend of
+              experiences fuels my passion for creating innovative and impactful
+              digital solutions.
+            </p>
+            {/* <p className="text-lg">
               I&apos;m an audio engineer, turned{" "}
               <span id="rainbow" className="font-bold text-xl">
                 software engineer
@@ -26,9 +42,11 @@ export default function Hero() {
               , based in Philadelphia, PA. This website is my personal
               portfolio, highlighting my work history, notable projects, and my
               profound love of tech.
-            </p>
+            </p> */}
             <div className="text-4xl grid place-items-center pt-32">
-              <FaChevronDown className="pulse" />
+              <FaChevronDown
+              // className="pulse"
+              />
             </div>
           </div>
         </div>
