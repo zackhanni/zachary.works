cab1fa01ad77d64361639a025af68d450fadf70e
Zack Hanni <zack.hannI@gmail.com>
2024-09-09 22:45:52 -0400

    Create TechnologyCard.tsx

---
+++ app/components/TechnologyCard.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function TechnologyCard({
+  image,
+  link,
+  name,
+}: {
+  image: string;
+  link: string;
+  name: string;
+}) {
+  return (
+    <Link href={link} target="_blank" key={name} className="mx-auto">
+      <div className="py-1 px-4 bg-white text-primary rounded-[25px] flex h-[65px] w-auto shadow-md hover:shadow-xl duration-300">
+        {/* {technology.name} */}
+        <Image
+          src={image}
+          width={101}
+          height={57}
+          alt={`${name} logo`}
+          className=" object-contain "
+        />
+      </div>
+    </Link>
+  );
+}
