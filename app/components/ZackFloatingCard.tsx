a6664d3177f0b0ec9d9a3cc4b607bfd44bab9912
Zack Hanni <zack.hannI@gmail.com>
2024-09-22 01:25:07 -0400

    made jazz cup design image full width on 'cup'

--- app/components/ZackFloatingCard.tsx
+++ app/components/ZackFloatingCard.tsx
@@ -16,7 +16,7 @@ export default function ZackFLoatingCard() {
   return (
     <Tilt
       options={tiltOptions}
-      className="col-3 hidden d-lg-block text-[#238493] bg-white rounded-lg sticky top-24 h-fit z-10"
+      className="col-3 hidden d-lg-block text-[#238493] bg-white rounded-lg sticky top-24 h-fit z-10 min-h-[350px]"
     >
       <div className="flex flex-col justify-start py-4 px-2 w-fit bg-white/80 rounded-lg">
         <h2 className="text-3xl font-bold">Zack Hanni</h2>
@@ -25,23 +25,7 @@ export default function ZackFLoatingCard() {
           I build exceptional and accessible digital experiences for the web.
         </p>
       </div>
-
-      <Image
-        src="/silly-straw.png"
-        width={100}
-        height={100}
-        alt="silly straw"
-        className="absolute -z-10 -top-80 rotate-[-20deg]"
-      />
-
-      <Image
-        src="/jazz-cup.png"
-        width={500}
-        height={300}
-        alt="jazz cup design"
-        className="py-2 bg-white"
-      />
-      <div className="flex justify-center py-4 space-x-4">
+      {/* <div className="flex justify-center space-x-4">
         <SocialMedia
           name="Linkedin"
           link="https://github.com/zackhanni"
@@ -57,6 +41,22 @@ export default function ZackFLoatingCard() {
           link="mailto:zack.hanni@gmail.com"
           icon={<IoMdMail />}
           className=""
+        />
+      </div> */}
+      <Image
+        src="/silly-straw.png"
+        width={100}
+        height={100}
+        alt="silly straw"
+        className="absolute -z-10 -top-80 rotate-[-20deg]"
+      />
+      <div>
+        <Image
+          src="/jazz-cup.png"
+          width={500}
+          height={300}
+          alt="jazz cup design"
+          className="py-2 bg-white absolute left-0 bottom-0 rounded-lg"
         />
       </div>
     </Tilt>
