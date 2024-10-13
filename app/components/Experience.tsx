fead22b88f07944a984adc0440711c6b4e5fdba0
Zack Hanni <zack.hannI@gmail.com>
2024-10-13 15:14:40 -0400

    created work card component

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import WorkData from "./ExperienceData";
 import ToolBubbles from "./ToolBubbles";
 import ZackFLoatingCard from "./ZackFloatingCard";
+import { useInView } from "react-intersection-observer";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Experience() {
   return (
@@ -10,7 +25,7 @@ export default function Experience() {
       className="bg-black text-slate-200 w-full flex justify-center pb-16"
     >
       <div className="container flex flex-col items-center justify-center bg-black z-10">
-        <div className="w-full flex items-center justify-center z-10 sticky top-0 backdrop-blur h-[90px]">
+        <div className="w-full flex items-center justify-center z-10 sticky top-0 backdrop-blur h-[110px]">
           <h2 className="text-5xl font-bold  text-slate-200 ">Experience</h2>
         </div>
 
@@ -21,7 +36,7 @@ export default function Experience() {
               <p>answer to - "So tell me about yourself" question</p>
             </div>
           </div> */}
-          <div className="col-lg-9 col-12 space-y-8">
+          {/* <div className="col-lg-9 col-12 space-y-8">
             {WorkData.map((job) => {
               return (
                 <div
@@ -46,9 +61,108 @@ export default function Experience() {
                 </div>
               );
             })}
+          </div> */}
+
+          {/* another one */}
+          <div className="col-lg-9 col-12 space-y-8">
+            {WorkData.map((job) => {
+              return (
+                <WorkCard
+                  title={job.title}
+                  company={job.company}
+                  bullets={job.bullets}
+                  dateRange={job.dateRange}
+                  description={job.description}
+                  location={job.location}
+                  tools={job.tools}
+                  key={job.title}
+                  content=""
+                  footer=""
+                />
+              );
+            })}
           </div>
+
+          {/* <div className="col-lg-9 col-12 space-y-8">
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem value="item-1">
+                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-2">
+                <AccordionTrigger>Is it styled?</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It comes with default styles that matches the other
+                  components&apos; aesthetic.
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-3">
+                <AccordionTrigger>Is it animated?</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It&apos;s animated by default, but you can disable it if
+                  you prefer.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div> */}
+
+          {/* breakup */}
         </div>
       </div>
     </section>
   );
 }
+
+const WorkCard = ({
+  title,
+  company,
+  dateRange,
+  location,
+  bullets,
+  description,
+  content,
+  footer,
+  tools,
+}: {
+  title: string;
+  company: string;
+  dateRange: string;
+  location: string;
+  bullets: ReactNode;
+  description: string[];
+  content: string;
+  footer: string;
+  tools: string[];
+}) => {
+  // const { ref, inView } = useInView({
+  //   threshold: 0.15,
+  //   triggerOnce: true,
+  // });
+
+  return (
+    <Card
+      className={`border-0 text-[#94A3B8] radial-gradient hover:bg-black/40 group `}
+    >
+      <div className="h-full">
+        <CardHeader>
+          <CardTitle className="mb-1 text-white">{title}</CardTitle>
+          <CardDescription className="mt-0 text-[#94A3B8]">
+            {company}
+            {dateRange}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          {description}
+          <hr className="my-6" />
+          {bullets}
+        </CardContent>
+        <CardFooter className="mt-auto">
+          {/* <p>{footer}</p> */}
+          <ToolBubbles tools={tools} />
+        </CardFooter>
+      </div>
+    </Card>
+  );
+};
