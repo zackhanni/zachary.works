2b47e9abc9024498618d50f373b29d0877235f27
Zack Hanni <zack.hannI@gmail.com>
2024-10-14 21:51:27 -0400

    adjusted card padding

--- app/components/ZackFloatingCard.tsx
+++ app/components/ZackFloatingCard.tsx
@@ -16,9 +16,9 @@ export default function ZackFLoatingCard() {
   return (
     <Tilt
       options={tiltOptions}
-      className="col-3 hidden d-lg-block text-[#238493] bg-white rounded-lg sticky top-[110px] h-fit z-10 min-h-[350px]"
+      className="col-3 hidden d-lg-block text-[#238493] bg-white rounded-lg sticky top-[110px] h-fit z-10 min-h-[350px] px-3"
     >
-      <div className="flex flex-col py-4 w-fit bg-white/80 rounded-lg">
+      <div className="flex flex-col py-3 w-fit bg-white/80 rounded-lg">
         <h2 className="text-3xl font-bold m-0">Zack Hanni</h2>
         <h3 className="text-sm m-0">Experienced Software Engineer</h3>
         <p className="m-0 py-3">
