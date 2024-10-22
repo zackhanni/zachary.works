12aa1de2e406125c0e69b0aaedefbe3781bbbb3b
Zack Hanni <zack.hannI@gmail.com>
2024-10-22 10:39:16 -0400

    added underline effect class to nav items

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -41,7 +41,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-9 w-max items-center justify-center text-white no-underline hover:underline rounded-full px-3 py-2 text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+  "group inline-flex h-9 w-max items-center justify-center text-white no-underline scale-underlined rounded-full px-2 py-2 font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
 );
 
 const NavigationMenuTrigger = React.forwardRef<
