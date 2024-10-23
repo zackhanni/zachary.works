3a71b658a8cb905e91c036ead229b341bb06c4b6
Zack Hanni <zack.hannI@gmail.com>
2024-10-23 13:29:59 -0400

    removed unused

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,12 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
-import HeroButtons from "./components/HeroButtons";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className="relative">
       <Hero />
-      {/* <HeroButtons /> */}
       <Experience />
       <Projects />
       <About />
