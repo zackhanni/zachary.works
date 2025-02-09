690301ab4d9e92344f9b5c6f9249041df3437834
Zack Hanni <zack.hannI@gmail.com>
2025-02-09 14:54:45 -0500

    made text visible only at top of page

--- app/components/SectionHeader.tsx
+++ app/components/SectionHeader.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 
 export default function SectionHeader({ title }: { title: string }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.getElementById(title);
+      if (header) {
+        const rect = header.getBoundingClientRect();
+        setIsVisible(rect.top <= 100); // Adjust pixels from top
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [title]);
+
   return (
     <div className="w-fit flex items-center justify-center z-20 sticky top-0 h-[90px]">
-      <h2 className="text-2xl font-semibold text-black ">{title}</h2>
+      <h2
+        id={title}
+        className={`text-2xl font-semibold transition-opacity duration-500 ${
+          isVisible ? "text-black opacity-100" : "opacity-0"
+        }`}
+      >
+        {title}
+      </h2>{" "}
     </div>
   );
 }
